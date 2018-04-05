14138f018217ec89a3044266628a63e0c13b2daa
Gustavo Moura <gnunes.moura@gmail.com>
2018-04-04 23:49:40 -0300

    add output chanel takes care of the

--- src/integration/service-builder.js
+++ src/integration/service-builder.js
@@ -1,38 +1,32 @@
-const errors = require('./errors')
+const build = require('./build')
+const basicService = require('./service').basicService
+
+const addInputChanel = builder => inputChanel => {
+  builder.service.inputChanel = () => new Promise(resolve => resolve(inputChanel()))
+  return builder
+}
+
+const addOutputChanel = builder => outputChanel => {
+  builder.service.outputChanel = output => output ? outputChanel(output) : output
+  return builder
+}
+
+const addTask = builder => task => {
+  builder.service.task = task
+  return builder
+}
 
 const serviceBuilder = () => {
-  const builder = {}
+  const builder = { service: basicService() }
 
-  builder.build = () => {
-    return () => builder.inputChanel()
-      .then(rec => {
-        if (!rec) throw new Error(errors.noMessage)
-        return rec
-      })
-      .then(rec => builder.task ? builder.task(rec) : rec)
-      .then(rec => builder.outputChanel ? builder.outputChanel(rec) : rec)
-      .catch(err => {
-        const normalError = Object.keys(errors).find(key => errors[key] === err.message)
-        if (!normalError) throw err
-      })
-  }
+  builder.addInputChanel = addInputChanel(builder)
+  builder.addTask = addTask(builder)
+  builder.addOutputChanel = addOutputChanel(builder)
 
-  builder.addInputChanel = inputChanel => {
-    builder.inputChanel = inputChanel
-    return builder
-  }
-
-  builder.addOutputChanel = outputChanel => {
-    builder.outputChanel = outputChanel
-    return builder
-  }
-
-  builder.addTask = task => {
-    builder.task = task
-    return builder
-  }
+  builder.build = build(builder)
 
   return builder
 }
+
 
 module.exports = serviceBuilder
